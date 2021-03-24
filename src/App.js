94e1476ad5d5b10f679692e4658914ce4b682090
RedisTheOne <redis123445@gmail.com>
2021-03-24 11:04:16 +0100

    device detection added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 
+import {
+  BrowserView,
+  MobileView
+} from 'react-device-detect';
+
 import { Container } from './components/Container';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Container />
-    </DndProvider>
+    <>
+      <BrowserView>
+        <DndProvider backend={HTML5Backend}>
+          <Container />
+        </DndProvider>
+      </BrowserView>
+      <MobileView>
+        <DndProvider backend={TouchBackend}>
+          <Container />
+        </DndProvider>
+      </MobileView>
+    </>
   );
 }
 
